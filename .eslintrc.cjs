5762ce74fd4e1a196040bb28764a5efb9657d940
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-05-03 13:16:52 +0200

    chore: setup husky and lint-staged
    Prettier was missing from past setup. A cleanup of App.tsx and main.tsx was required.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,78 +1,77 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb-typescript',
-    'prettier',
-  ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.eslint.json',
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    // Disable React import.
-    'react/react-in-jsx-scope': 0,
-    // Sort imports.
-    'sort-imports': [
-      'warn',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-      },
-    ],
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          ['external', 'builtin'],
-          'internal',
-          ['sibling', 'parent'],
-          'index',
-        ],
-        pathGroups: [
-          {
-            pattern: '@(react|react-native)',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: 'src/**',
-            group: 'internal',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['internal', 'react'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    // Require default props.
-    'react/require-default-props': [
-      'error',
-      {
-        forbidDefaultForRequired: true,
-        functions: 'defaultArguments',
-      },
-    ],
-    // Allow use of arrow functions.
-    'arrow-body-style': ['warn', 'always'],
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: ['function-declaration', 'arrow-function'],
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'airbnb-typescript',
+		'prettier',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.eslint.json',
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		// Disable React import.
+		'react/react-in-jsx-scope': 0,
+		// Sort imports.
+		'sort-imports': [
+			'warn',
+			{
+				ignoreCase: true,
+				ignoreDeclarationSort: true,
+			},
+		],
+		'import/order': [
+			'warn',
+			{
+				groups: [
+					['external', 'builtin'],
+					'internal',
+					['sibling', 'parent'],
+					'index',
+				],
+				pathGroups: [
+					{
+						pattern: '@(react|react-native)',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: 'src/**',
+						group: 'internal',
+					},
+				],
+				pathGroupsExcludedImportTypes: ['internal', 'react'],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
+		// Require default props.
+		'react/require-default-props': [
+			'error',
+			{
+				forbidDefaultForRequired: true,
+			},
+		],
+		// Allow use of arrow functions.
+		'arrow-body-style': ['warn', 'always'],
+		'react/function-component-definition': [
+			'error',
+			{
+				namedComponents: ['function-declaration', 'arrow-function'],
+				unnamedComponents: 'arrow-function',
+			},
+		],
+	},
 }
